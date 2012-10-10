c8154f8e77ed73056ff6ba130d7cbde060eee88b
Niall Casey <npcasey@tcd.ie>
2012-10-10 10:41:33 +0100

    Planet of the day function
    Allows a new random planet every day, but keeps the same planet for the
    entire day

--- application/controllers/planets.php
+++ application/controllers/planets.php
@@ -29,6 +29,34 @@ class Planets extends CI_Controller {
 		$this->load->view('panel',$planet);
 	}
 	
+	public function planet_of_the_day()
+	{
+		$skyhook = json_decode(  
+			file_get_contents('http://exoapi.com/api/skyhook/planets/all?fields=[_id,image]')
+		);
+		$planets = $skyhook->response->results;
+		
+		//Only interested in planets that have images
+		$filtered = array_values(array_filter($planets, array($this, 'has_image')));
+	
+		//pick a random palnet, using the current date as a seed
+		@mt_srand(date("Ymd"));
+		$j = @mt_rand(0, count($filtered)-1);
+		
+		//Reset the seed, so subsiquent calls to mt_rand are unpredictable again
+		@mt_srand();
+
+		$selected = $filtered[$j];
+
+		//Get some finer details frm the exoplanet catalogue
+
+		$this->load->model('Planet_Model');
+		$planet = $this->Planet_Model->get_planet($selected->_id);
+
+
+		$this->load->view('panel',$planet);
+	}
+	
 	private function has_image($planet)
 	{
 		$trimmed = $planet->image;
